f419c662d4946f3b4a2ac2edc8423a91bc1c10ba
merleandrade <andrade.merle@gmail.com>
2022-07-13 16:01:19 +0200

    Commit Initial

---
+++ src/test/java/com/example/gitmain/GitMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
